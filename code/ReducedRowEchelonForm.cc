cc0c114f67d6c149ef0173d6aded5ef29465ec37
Jaehyun Park <jpark@cs.stanford.edu>
2015-10-24 14:28:40 -0700

    removing tabs

--- code/ReducedRowEchelonForm.cc
+++ code/ReducedRowEchelonForm.cc
@@ -22,53 +22,53 @@ typedef vector<T> VT;
 typedef vector<VT> VVT;
 
 int rref(VVT &a) {
-	int n = a.size();
-	int m = a[0].size();
-	int r = 0;
-	for (int c = 0; c < m && r < n; c++) {
-		int j = r;
-		for (int i = r + 1; i < n; i++)
-			if (fabs(a[i][c]) > fabs(a[j][c])) j = i;
-		if (fabs(a[j][c]) < EPSILON) continue;
-		swap(a[j], a[r]);
+  int n = a.size();
+  int m = a[0].size();
+  int r = 0;
+  for (int c = 0; c < m && r < n; c++) {
+    int j = r;
+    for (int i = r + 1; i < n; i++)
+      if (fabs(a[i][c]) > fabs(a[j][c])) j = i;
+    if (fabs(a[j][c]) < EPSILON) continue;
+    swap(a[j], a[r]);
 
-		T s = 1.0 / a[r][c];
-		for (int j = 0; j < m; j++) a[r][j] *= s;
-		for (int i = 0; i < n; i++) if (i != r) {
-			T t = a[i][c];
-			for (int j = 0; j < m; j++) a[i][j] -= t * a[r][j];
-		}
-		r++;
-	}
-	return r;
+    T s = 1.0 / a[r][c];
+    for (int j = 0; j < m; j++) a[r][j] *= s;
+    for (int i = 0; i < n; i++) if (i != r) {
+      T t = a[i][c];
+      for (int j = 0; j < m; j++) a[i][j] -= t * a[r][j];
+    }
+    r++;
+  }
+  return r;
 }
 
 int main() {
-	const int n = 5, m = 4;
-	double A[n][m] = {
-		{16,  2,  3, 13},
-		{ 5, 11, 10,  8},
-		{ 9,  7,  6, 12},
-		{ 4, 14, 15,  1},
-		{13, 21, 21, 13}};
-	VVT a(n);
-	for (int i = 0; i < n; i++)
-		a[i] = VT(A[i], A[i] + m);
+  const int n = 5, m = 4;
+  double A[n][m] = {
+    {16,  2,  3, 13},
+    { 5, 11, 10,  8},
+    { 9,  7,  6, 12},
+    { 4, 14, 15,  1},
+    {13, 21, 21, 13}};
+  VVT a(n);
+  for (int i = 0; i < n; i++)
+    a[i] = VT(A[i], A[i] + m);
 
-	int rank = rref(a);
+  int rank = rref(a);
 
-	// expected: 3
-	cout << "Rank: " << rank << endl;
+  // expected: 3
+  cout << "Rank: " << rank << endl;
 
-	// expected: 1 0 0 1 
-	//           0 1 0 3 
-	//           0 0 1 -3 
-	//           0 0 0 3.10862e-15
-	//           0 0 0 2.22045e-15
-	cout << "rref: " << endl;
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 4; j++)
-			cout << a[i][j] << ' ';
-		cout << endl;
-	}
+  // expected: 1 0 0 1 
+  //           0 1 0 3 
+  //           0 0 1 -3 
+  //           0 0 0 3.10862e-15
+  //           0 0 0 2.22045e-15
+  cout << "rref: " << endl;
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 4; j++)
+      cout << a[i][j] << ' ';
+    cout << endl;
+  }
 }
--- code/Simplex.cc
+++ code/Simplex.cc
@@ -33,93 +33,93 @@ const DOUBLE EPS = 1e-9;
 DOUBLE Dabs(DOUBLE x) { return x < 0 ? -x : x; }
 
 struct LPSolver {
-	int m, n;
-	VI B, N;
-	VVD D;
+  int m, n;
+  VI B, N;
+  VVD D;
 
-	LPSolver(const VVD &A, const VD &b, const VD &c) :
-		m(b.size()), n(c.size()), N(n + 1), B(m), D(m + 2, VD(n + 2)) {
-		for (int i = 0; i < m; i++) for (int j = 0; j < n; j++) D[i][j] = A[i][j];
-		for (int i = 0; i < m; i++) { B[i] = n + i; D[i][n] = -1; D[i][n + 1] = b[i]; }
-		for (int j = 0; j < n; j++) { N[j] = j; D[m][j] = -c[j]; }
-		N[n] = -1; D[m + 1][n] = 1;
-	}
+  LPSolver(const VVD &A, const VD &b, const VD &c) :
+    m(b.size()), n(c.size()), N(n + 1), B(m), D(m + 2, VD(n + 2)) {
+    for (int i = 0; i < m; i++) for (int j = 0; j < n; j++) D[i][j] = A[i][j];
+    for (int i = 0; i < m; i++) { B[i] = n + i; D[i][n] = -1; D[i][n + 1] = b[i]; }
+    for (int j = 0; j < n; j++) { N[j] = j; D[m][j] = -c[j]; }
+    N[n] = -1; D[m + 1][n] = 1;
+  }
 
-	void Pivot(int r, int s) {
-		for (int i = 0; i < m + 2; i++) if (i != r)
-			for (int j = 0; j < n + 2; j++) if (j != s)
-				D[i][j] -= D[r][j] * D[i][s] / D[r][s];
-		for (int j = 0; j < n + 2; j++) if (j != s) D[r][j] /= D[r][s];
-		for (int i = 0; i < m + 2; i++) if (i != r) D[i][s] /= -D[r][s];
-		D[r][s] = 1.0 / D[r][s];
-		swap(B[r], N[s]);
-	}
+  void Pivot(int r, int s) {
+    for (int i = 0; i < m + 2; i++) if (i != r)
+      for (int j = 0; j < n + 2; j++) if (j != s)
+        D[i][j] -= D[r][j] * D[i][s] / D[r][s];
+    for (int j = 0; j < n + 2; j++) if (j != s) D[r][j] /= D[r][s];
+    for (int i = 0; i < m + 2; i++) if (i != r) D[i][s] /= -D[r][s];
+    D[r][s] = 1.0 / D[r][s];
+    swap(B[r], N[s]);
+  }
 
-	bool Simplex(int phase) {
-		int x = phase == 1 ? m + 1 : m;
-		while (true) {
-			int s = -1;
-			for (int j = 0; j <= n; j++) {
-				if (phase == 2 && N[j] == -1) continue;
-				if (s == -1 || D[x][j] < D[x][s] || Dabs(D[x][j] - D[x][s]) < EPS && N[j] < N[s]) s = j;
-			}
-			if (D[x][s] > -EPS) return true;
-			int r = -1;
-			for (int i = 0; i < m; i++) {
-				if (D[i][s] < EPS) continue;
-				if (r == -1 || D[i][n + 1] / D[i][s] < D[r][n + 1] / D[r][s] ||
-					Dabs((D[i][n + 1] / D[i][s]) - (D[r][n + 1] / D[r][s])) < EPS && B[i] < B[r]) r = i;
-			}
-			if (r == -1) return false;
-			Pivot(r, s);
-		}
-	}
+  bool Simplex(int phase) {
+    int x = phase == 1 ? m + 1 : m;
+    while (true) {
+      int s = -1;
+      for (int j = 0; j <= n; j++) {
+        if (phase == 2 && N[j] == -1) continue;
+        if (s == -1 || D[x][j] < D[x][s] || Dabs(D[x][j] - D[x][s]) < EPS && N[j] < N[s]) s = j;
+      }
+      if (D[x][s] > -EPS) return true;
+      int r = -1;
+      for (int i = 0; i < m; i++) {
+        if (D[i][s] < EPS) continue;
+        if (r == -1 || D[i][n + 1] / D[i][s] < D[r][n + 1] / D[r][s] ||
+          Dabs((D[i][n + 1] / D[i][s]) - (D[r][n + 1] / D[r][s])) < EPS && B[i] < B[r]) r = i;
+      }
+      if (r == -1) return false;
+      Pivot(r, s);
+    }
+  }
 
-	DOUBLE Solve(VD &x) {
-		int r = 0;
-		for (int i = 1; i < m; i++) if (D[i][n + 1] < D[r][n + 1]) r = i;
-		if (D[r][n + 1] < -EPS) {
-			Pivot(r, n);
-			if (!Simplex(1) || D[m + 1][n + 1] < -EPS) return -numeric_limits<DOUBLE>::infinity();
-			for (int i = 0; i < m; i++) if (B[i] == -1) {
-				int s = -1;
-				for (int j = 0; j <= n; j++)
-					if (s == -1 || D[i][j] < D[i][s] || D[i][j] == D[i][s] && N[j] < N[s]) s = j;
-				Pivot(i, s);
-			}
-		}
-		if (!Simplex(2)) return numeric_limits<DOUBLE>::infinity();
-		x = VD(n);
-		for (int i = 0; i < m; i++) if (B[i] < n) x[B[i]] = D[i][n + 1];
-		return D[m][n + 1];
-	}
+  DOUBLE Solve(VD &x) {
+    int r = 0;
+    for (int i = 1; i < m; i++) if (D[i][n + 1] < D[r][n + 1]) r = i;
+    if (D[r][n + 1] < -EPS) {
+      Pivot(r, n);
+      if (!Simplex(1) || D[m + 1][n + 1] < -EPS) return -numeric_limits<DOUBLE>::infinity();
+      for (int i = 0; i < m; i++) if (B[i] == -1) {
+        int s = -1;
+        for (int j = 0; j <= n; j++)
+          if (s == -1 || D[i][j] < D[i][s] || D[i][j] == D[i][s] && N[j] < N[s]) s = j;
+        Pivot(i, s);
+      }
+    }
+    if (!Simplex(2)) return numeric_limits<DOUBLE>::infinity();
+    x = VD(n);
+    for (int i = 0; i < m; i++) if (B[i] < n) x[B[i]] = D[i][n + 1];
+    return D[m][n + 1];
+  }
 };
 
 int main() {
 
-	const int m = 4;
-	const int n = 3;
-	DOUBLE _A[m][n] = {
-		{ 6, -1, 0 },
-		{ -1, -5, 0 },
-		{ 1, 5, 1 },
-		{ -1, -5, -1 }
-	};
-	DOUBLE _b[m] = { 10, -4, 5, -5 };
-	DOUBLE _c[n] = { 1, -1, 0 };
+  const int m = 4;
+  const int n = 3;
+  DOUBLE _A[m][n] = {
+    { 6, -1, 0 },
+    { -1, -5, 0 },
+    { 1, 5, 1 },
+    { -1, -5, -1 }
+  };
+  DOUBLE _b[m] = { 10, -4, 5, -5 };
+  DOUBLE _c[n] = { 1, -1, 0 };
 
-	VVD A(m);
-	VD b(_b, _b + m);
-	VD c(_c, _c + n);
-	for (int i = 0; i < m; i++) A[i] = VD(_A[i], _A[i] + n);
+  VVD A(m);
+  VD b(_b, _b + m);
+  VD c(_c, _c + n);
+  for (int i = 0; i < m; i++) A[i] = VD(_A[i], _A[i] + n);
 
-	LPSolver solver(A, b, c);
-	VD x;
-	DOUBLE value = solver.Solve(x);
+  LPSolver solver(A, b, c);
+  VD x;
+  DOUBLE value = solver.Solve(x);
 
-	cerr << "VALUE: " << value << endl; // VALUE: 1.29032
-	cerr << "SOLUTION:"; // SOLUTION: 1.74194 0.451613 1
-	for (size_t i = 0; i < x.size(); i++) cerr << " " << x[i];
-	cerr << endl;
-	return 0;
+  cerr << "VALUE: " << value << endl; // VALUE: 1.29032
+  cerr << "SOLUTION:"; // SOLUTION: 1.74194 0.451613 1
+  for (size_t i = 0; i < x.size(); i++) cerr << " " << x[i];
+  cerr << endl;
+  return 0;
 }
