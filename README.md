7ac0c290ea6e16a5e4ef533b6796c0fe382ef2bb
Jaehyun Park <jpark@cs.stanford.edu>
2016-05-27 15:38:56 -0700

    dijkstra and number theoretic functions bug fix

---
+++ README.md
@@ -0,0 +1,13 @@
+Stanford ACM-ICPC
+=================
+
+This is a repository for the Stanford ACM-ICPC teams.
+It currently hosts (a) the team notebook, and (b) complete lecture slides for [CS 97SI](http://stanford.edu/class/cs97si/).
+
+The team notebook is compiled from codes written by previous Stanford team members and coaches. Currently, the steps for generating the HTML and PDF files are outdated and somewhat complicated. We hope to make this process a lot simpler and hopefully cross-platform.
+
+Links
+=====
+[Stanford Local Programming Contest](http://cs.stanford.edu/group/acm/SLPC/)
+[CS 97SI: Introduction to Programming Contests](http://stanford.edu/class/cs97si/)
+[Stanford ACM-ICPC e-mail list](https://mailman.stanford.edu/mailman/listinfo/stanford-acm-icpc)
--- code/Euclid.cc
+++ code/Euclid.cc
@@ -9,123 +9,149 @@
 using namespace std;
 
 typedef vector<int> VI;
-typedef pair<int,int> PII;
+typedef pair<int, int> PII;
 
 // return a % b (positive value)
 int mod(int a, int b) {
-  return ((a%b)+b)%b;
+	return ((a%b) + b) % b;
 }
 
 // computes gcd(a,b)
 int gcd(int a, int b) {
-  int tmp;
-  while(b){a%=b; tmp=a; a=b; b=tmp;}
-  return a;
+	while (b) { int t = a%b; a = b; b = t; }
+	return a;
 }
 
 // computes lcm(a,b)
 int lcm(int a, int b) {
-  return a/gcd(a,b)*b;
+	return a / gcd(a, b)*b;
 }
 
-// returns d = gcd(a,b); finds x,y such that d = ax + by
-int extended_euclid(int a, int b, int &x, int &y) {  
-  int xx = y = 0;
-  int yy = x = 1;
-  while (b) {
-    int q = a/b;
-    int t = b; b = a%b; a = t;
-    t = xx; xx = x-q*xx; x = t;
-    t = yy; yy = y-q*yy; y = t;
-  }
-  return a;
+// (a^b) mod m via successive squaring
+int powermod(int a, int b, int m)
+{
+	int ret = 1;
+	while (b)
+	{
+		if (b & 1) ret = mod(ret*a, m);
+		a = mod(a*a, m);
+		b >>= 1;
+	}
+	return ret;
+}
+
+// returns g = gcd(a, b); finds x, y such that d = ax + by
+int extended_euclid(int a, int b, int &x, int &y) {
+	int xx = y = 0;
+	int yy = x = 1;
+	while (b) {
+		int q = a / b;
+		int t = b; b = a%b; a = t;
+		t = xx; xx = x - q*xx; x = t;
+		t = yy; yy = y - q*yy; y = t;
+	}
+	return a;
 }
 
 // finds all solutions to ax = b (mod n)
 VI modular_linear_equation_solver(int a, int b, int n) {
-  int x, y;
-  VI solutions;
-  int d = extended_euclid(a, n, x, y);
-  if (!(b%d)) {
-    x = mod (x*(b/d), n);
-    for (int i = 0; i < d; i++)
-      solutions.push_back(mod(x + i*(n/d), n));
-  }
-  return solutions;
+	int x, y;
+	VI ret;
+	int g = extended_euclid(a, n, x, y);
+	if (!(b%g)) {
+		x = mod(x*(b / g), n);
+		for (int i = 0; i < g; i++)
+			ret.push_back(mod(x + i*(n / g), n));
+	}
+	return ret;
 }
 
 // computes b such that ab = 1 (mod n), returns -1 on failure
 int mod_inverse(int a, int n) {
-  int x, y;
-  int d = extended_euclid(a, n, x, y);
-  if (d > 1) return -1;
-  return mod(x,n);
+	int x, y;
+	int g = extended_euclid(a, n, x, y);
+	if (g > 1) return -1;
+	return mod(x, n);
 }
 
 // Chinese remainder theorem (special case): find z such that
-// z % x = a, z % y = b.  Here, z is unique modulo M = lcm(x,y).
-// Return (z,M).  On failure, M = -1.
-PII chinese_remainder_theorem(int x, int a, int y, int b) {
-  int s, t;
-  int d = extended_euclid(x, y, s, t);
-  if (a%d != b%d) return make_pair(0, -1);
-  return make_pair(mod(s*b*x+t*a*y,x*y)/d, x*y/d);
+// z % m1 = r1, z % m2 = r2.  Here, z is unique modulo M = lcm(m1, m2).
+// Return (z, M).  On failure, M = -1.
+PII chinese_remainder_theorem(int m1, int r1, int m2, int r2) {
+	int s, t;
+	int g = extended_euclid(m1, m2, s, t);
+	if (r1%g != r2%g) return make_pair(0, -1);
+	return make_pair(mod(s*r2*m1 + t*r1*m2, m1*m2) / g, m1*m2 / g);
 }
 
 // Chinese remainder theorem: find z such that
-// z % x[i] = a[i] for all i.  Note that the solution is
-// unique modulo M = lcm_i (x[i]).  Return (z,M).  On 
-// failure, M = -1.  Note that we do not require the a[i]'s
+// z % m[i] = r[i] for all i.  Note that the solution is
+// unique modulo M = lcm_i (m[i]).  Return (z, M). On 
+// failure, M = -1. Note that we do not require the a[i]'s
 // to be relatively prime.
-PII chinese_remainder_theorem(const VI &x, const VI &a) {
-  PII ret = make_pair(a[0], x[0]);
-  for (int i = 1; i < x.size(); i++) {
-    ret = chinese_remainder_theorem(ret.second, ret.first, x[i], a[i]);
-    if (ret.second == -1) break;
-  }
-  return ret;
+PII chinese_remainder_theorem(const VI &m, const VI &r) {
+	PII ret = make_pair(r[0], m[0]);
+	for (int i = 1; i < m.size(); i++) {
+		ret = chinese_remainder_theorem(ret.second, ret.first, m[i], r[i]);
+		if (ret.second == -1) break;
+	}
+	return ret;
 }
 
-// computes x and y such that ax + by = c; on failure, x = y =-1
-void linear_diophantine(int a, int b, int c, int &x, int &y) {
-  int d = gcd(a,b);
-  if (c%d) {
-    x = y = -1;
-  } else {
-    x = c/d * mod_inverse(a/d, b/d);
-    y = (c-a*x)/b;
-  }
+// computes x and y such that ax + by = c
+// returns whether the solution exists
+bool linear_diophantine(int a, int b, int c, int &x, int &y) {
+	if (!a && !b)
+	{
+		if (c) return false;
+		x = 0; y = 0;
+		return true;
+	}
+	if (!a)
+	{
+		if (c % b) return false;
+		x = 0; y = c / b;
+		return true;
+	}
+	if (!b)
+	{
+		if (c % a) return false;
+		x = c / a; y = 0;
+		return true;
+	}
+	int g = gcd(a, b);
+	if (c % g) return false;
+	x = c / g * mod_inverse(a / g, b / g);
+	y = (c - a*x) / b;
+	return true;
 }
 
 int main() {
-  
-  // expected: 2
-  cout << gcd(14, 30) << endl;
-  
-  // expected: 2 -2 1
-  int x, y;
-  int d = extended_euclid(14, 30, x, y);
-  cout << d << " " << x << " " << y << endl;
-  
-  // expected: 95 45
-  VI sols = modular_linear_equation_solver(14, 30, 100);
-  for (int i = 0; i < (int) sols.size(); i++) cout << sols[i] << " "; 
-  cout << endl;
-  
-  // expected: 8
-  cout << mod_inverse(8, 9) << endl;
-  
-  // expected: 23 56
-  //           11 12
-  int xs[] = {3, 5, 7, 4, 6};
-  int as[] = {2, 3, 2, 3, 5};
-  PII ret = chinese_remainder_theorem(VI (xs, xs+3), VI(as, as+3));
-  cout << ret.first << " " << ret.second << endl;
-  ret = chinese_remainder_theorem (VI(xs+3, xs+5), VI(as+3, as+5));
-  cout << ret.first << " " << ret.second << endl;
-  
-  // expected: 5 -15
-  linear_diophantine(7, 2, 5, x, y);
-  cout << x << " " << y << endl;
+	// expected: 2
+	cout << gcd(14, 30) << endl;
 
+	// expected: 2 -2 1
+	int x, y;
+	int g = extended_euclid(14, 30, x, y);
+	cout << g << " " << x << " " << y << endl;
+
+	// expected: 95 451
+	VI sols = modular_linear_equation_solver(14, 30, 100);
+	for (int i = 0; i < sols.size(); i++) cout << sols[i] << " ";
+	cout << endl;
+
+	// expected: 8
+	cout << mod_inverse(8, 9) << endl;
+
+	// expected: 23 105
+	//           11 12
+	PII ret = chinese_remainder_theorem(VI({ 3, 5, 7 }), VI({ 2, 3, 2 }));
+	cout << ret.first << " " << ret.second << endl;
+	ret = chinese_remainder_theorem(VI({ 4, 6 }), VI({ 3, 5 }));
+	cout << ret.first << " " << ret.second << endl;
+
+	// expected: 5 -15
+	if (!linear_diophantine(7, 2, 5, x, y)) cout << "ERROR" << endl;
+	cout << x << " " << y << endl;
+	return 0;
 }
