11ba6dcef0b5fa4b372ae9dd780242c16574f3d8
Jaehyun Park <jpark@cs.stanford.edu>
2017-03-02 14:22:23 -0800

    fixing KMP behavior, pattern length=1 bug

--- code/KMP.cc
+++ code/KMP.cc
@@ -1,7 +1,7 @@
 /*
-Searches for the string w in the string s (of length k). Returns the
-0-based index of the first match (k if no match is found). Algorithm
-runs in O(k) time.
+Finds all occurrences of the pattern string p within the
+text string t. Running time is O(n + m), where n and m
+are the lengths of p and t, respecitvely.
 */
 
 #include <iostream>
@@ -12,53 +12,39 @@ using namespace std;
 
 typedef vector<int> VI;
 
-void buildTable(string& w, VI& t)
+void buildPi(string& p, VI& pi)
 {
-  t = VI(w.length());  
-  int i = 2, j = 0;
-  t[0] = -1; t[1] = 0;
-  
-  while(i < w.length())
-  {
-    if(w[i-1] == w[j]) { t[i] = j+1; i++; j++; }
-    else if(j > 0) j = t[j];
-    else { t[i] = 0; i++; }
+  pi = VI(p.length());
+  int k = -2;
+  for(int i = 0; i < p.length(); i++) {
+    while(k >= -1 && p[k+1] != p[i])
+      k = (k == -1) ? -2 : pi[k];
+    pi[i] = ++k;
   }
 }
 
-int KMP(string& s, string& w)
+int KMP(string& t, string& p)
 {
-  int m = 0, i = 0;
-  VI t;
-  
-  buildTable(w, t);  
-  while(m+i < s.length())
-  {
-    if(w[i] == s[m+i])
-    {
-      i++;
-      if(i == w.length()) return m;
+  VI pi;
+  buildPi(p, pi);
+  int k = -1;
+  for(int i = 0; i < t.length(); i++) {
+    while(k >= -1 && p[k+1] != t[i])
+      k = (k == -1) ? -2 : pi[k];
+    k++;
+    if(k == p.length() - 1) {
+      // p matches t[i-m+1, ..., i]
+      cout << "matched at index " << i-k << ": ";
+      cout << t.substr(i-k, p.length()) << endl;
+      k = (k == -1) ? -2 : pi[k];
     }
-    else
-    {
-      m += i-t[i];
-      if(i > 0) i = t[i];
-    }
-  }  
-  return s.length();
+  }
+  return 0;
 }
 
 int main()
 {
-  string a = (string) "The example above illustrates the general technique for assembling "+
-    "the table with a minimum of fuss. The principle is that of the overall search: "+
-    "most of the work was already done in getting to the current position, so very "+
-    "little needs to be done in leaving it. The only minor complication is that the "+
-    "logic which is correct late in the string erroneously gives non-proper "+
-    "substrings at the beginning. This necessitates some initialization code.";
-  
-  string b = "table";
-  
-  int p = KMP(a, b);
-  cout << p << ": " << a.substr(p, b.length()) << " " << b << endl;
+  string a = "AABAACAADAABAABA", b = "AABA";
+  KMP(a, b); // expected matches at: 0, 9, 12
+  return 0;
 }
