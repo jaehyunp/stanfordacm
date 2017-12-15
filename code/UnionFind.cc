cbac97c0ca7bf7b4e32ca6569248340eed1f557d
Madhur Chauhan <madhur4127@gmail.com>
2017-12-15 15:17:30 +0530

    New implementation for Union Find
    A new suggestion for implementation of union find data structure. 
    No need to code 'initialization' in main(), and it makes code debugging and usage easy! Hiding details of implementation much like library like usage.

--- code/UnionFind.cc
+++ code/UnionFind.cc
@@ -1,16 +1,20 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-int find(vector<int> &C, int x) { return (C[x] == x) ? x : C[x] = find(C, C[x]); }
-void merge(vector<int> &C, int x, int y) { C[find(C, x)] = find(C, y); }
+///////////////////// UNION FIND ////////////////////////
+typedef struct _dsu{
+  //USAGE: DSU <name>(<size>); <name>.merge(x,y); <name>.find(x);
+  vector<int> par;
+  _dsu(int n)       {par.resize(n); for(int i=0;i<n;++i) par[i]=i;}
+  int find(int x)   {return x==par[x]?x:par[x]=find(par[x]);}
+  void merge(int x, int y)  {par[find(x)]=par(y);}
+} DSU;
+
 int main()
 {
-	int n = 5;
-	vector<int> C(n);
-	for (int i = 0; i < n; i++) C[i] = i;
-	merge(C, 0, 2);
-	merge(C, 1, 0);
-	merge(C, 3, 4);
-	for (int i = 0; i < n; i++) cout << i << " " << find(C, i) << endl;
+	int n = 5; DSU dsu(n);
+	dsu.merge(0, 2);
+	dsu.merge(1, 0);
+	dsu.merge(3, 4);
+	for (int i = 0; i < n; i++) cout << i << " " << dsu.find(i) << endl;
 	return 0;
 }
