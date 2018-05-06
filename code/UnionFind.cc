3310168870a8505b3c438423dba54435489dc2c6
Jaehyun Park <jpark@cs.stanford.edu>
2018-05-05 22:41:14 -0700

    struct-ify union find

--- code/UnionFind.cc
+++ code/UnionFind.cc
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int find(vector<int> &C, int x) { return (C[x] == x) ? x : C[x] = find(C, C[x]); }
-void merge(vector<int> &C, int x, int y) { C[find(C, x)] = find(C, y); }
+struct UnionFind {
+    vector<int> C;
+    UnionFind(int n) : C(n) { for (int i = 0; i < n; i++) C[i] = i; }
+    int find(int x) { return (C[x] == x) ? x : C[x] = find(C[x]); }
+    void merge(int x, int y) { C[find(x)] = find(y); }
+};
 int main()
 {
-	int n = 5;
-	vector<int> C(n);
-	for (int i = 0; i < n; i++) C[i] = i;
-	merge(C, 0, 2);
-	merge(C, 1, 0);
-	merge(C, 3, 4);
-	for (int i = 0; i < n; i++) cout << i << " " << find(C, i) << endl;
-	return 0;
+    int n = 5;
+    UnionFind uf(n);
+    uf.merge(0, 2);
+    uf.merge(1, 0);
+    uf.merge(3, 4);
+    for (int i = 0; i < n; i++) cout << i << " " << uf.find(i) << endl;
+    return 0;
 }
